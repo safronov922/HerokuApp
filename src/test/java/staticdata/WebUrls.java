b383b9c5574904f5bc29a669059d7a46bdd0fdd6
safronov922 <safronov922@yandex.ru>
2022-02-15 16:58:20 +0300

    Creating tests

---
+++ src/test/java/staticdata/WebUrls.java
@@ -0,0 +1,6 @@
+package staticdata;
+
+public class WebUrls {
+    public static final String ADD_REMOVE = "http://the-internet.herokuapp.com/add_remove_elements/";
+    public  static final String DATA_TABLES = "http://the-internet.herokuapp.com/tables";
+}
